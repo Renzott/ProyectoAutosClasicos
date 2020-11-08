3994bb52ec012406ebdc41fab3ac2b4676cba3ed
renzott <renzottaffairs@gmail.com>
2020-11-07 23:47:36 -0500

    first commit

---
+++ src/edu/cibertec/beans/CombustibleDTO.java
@@ -0,0 +1,48 @@
+package edu.cibertec.beans;
+
+import java.io.Serializable;
+
+public class CombustibleDTO implements Serializable{
+	
+	
+	private String CodigoCombustible;
+	private String NombreCombustible;
+	
+	
+	public CombustibleDTO() {}
+
+	public CombustibleDTO(String codigoCombustible, String nombreCombustible) {
+		super();
+		CodigoCombustible = codigoCombustible;
+		NombreCombustible = nombreCombustible;
+	}
+
+
+
+
+
+
+
+	public String getCodigoCombustible() {
+		return CodigoCombustible;
+	}
+	public void setCodigoCombustible(String codigoCombustible) {
+		CodigoCombustible = codigoCombustible;
+	}
+
+
+	public String getNombreCombustible() {
+		return NombreCombustible;
+	}
+	public void setNombreCombustible(String nombreCombustible) {
+		NombreCombustible = nombreCombustible;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/edu/cibertec/beans/TipoAutoDTO.java
@@ -0,0 +1,50 @@
+package edu.cibertec.beans;
+
+import java.io.Serializable;
+
+public class TipoAutoDTO implements Serializable{
+	
+	private String CodigoTipoAuto;
+	private String NombreTipoAuto;
+	
+	
+	public TipoAutoDTO() {}
+	
+	
+	public TipoAutoDTO(String codigoTipoAuto, String nombreTipoAuto) {
+		super();
+		CodigoTipoAuto = codigoTipoAuto;
+		NombreTipoAuto = nombreTipoAuto;
+	}
+
+
+
+
+
+
+
+	public String getCodigoTipoAuto() {
+		return CodigoTipoAuto;
+	}
+	public void setCodigoTipoAuto(String codigoTipoAuto) {
+		CodigoTipoAuto = codigoTipoAuto;
+	}
+	
+	
+	public String getNombreTipoAuto() {
+		return NombreTipoAuto;
+	}
+	public void setNombreTipoAuto(String nombreTipoAuto) {
+		NombreTipoAuto = nombreTipoAuto;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
